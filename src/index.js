a839163ecf42af886127ca73c69293a8ced8afa1
Michail <tyuris@icloud.com>
2025-08-01 23:03:10 +0300

    init:task initilization and solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  // throw new Error('Not implemented')
+  const num = n >= 0 ? n : -1 * n;
+  const arr = num.toString().split('');
+  return Number(arr.reverse().join(''));
 };
